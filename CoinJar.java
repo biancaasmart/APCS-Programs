783901f7cf92c7e36fb3297675d5603761657943
biancaasmart <biancaasmart@gmail.com>
2017-01-18 13:38:41 -0500

    Add files via upload

---
+++ CoinJar.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class CoinJar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CoinJar
+{
+   public static void main(String[] args)
+   {
+       int n = 14;
+       double total = 1.05;
+       double quarter = .25;
+       double penny = .01;
+       double dime = .1;
+       double nickle = .05;
+       for(int q = .25; q < 4; q++)
+       {
+           for(int 
+        }
+    }
+}
