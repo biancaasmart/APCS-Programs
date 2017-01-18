efc5c149a25c9f819853067f6b8ff498a8cee268
biancaasmart <biancaasmart@gmail.com>
2017-01-18 11:43:10 -0500

    Add files via upload

---
+++ SumAverage.java
@@ -0,0 +1,30 @@
+/**
+ * Bianca Smart
+ * Watauga High School
+ * 9/19/16
+ * Sum & Average
+ * This program calculates and outputs the average of four numbers
+ * 
+ * Learned: N/a
+ * Difficulties: N/a
+ * 
+ */
+public class SumAverage
+{
+    public static void main(String[] args)
+    {
+        int one = 475, two = 821, three = 369, four = 562;
+        int sum = one+two+three+four;
+        double average = sum/4.0;
+        System.out.println("Thank you for entering your four numbers! Your results are:");
+        System.out.println("\nThe sum of the four numbers is "+sum+" and the average is "+average+".");
+      
+    }
+}
+/*
+ * Sample output:
+ * Thank you for entering your four numbers! Your results are:
+
+The sum of the four numbers is 2227 and the average is 556.75.
+
+ */
