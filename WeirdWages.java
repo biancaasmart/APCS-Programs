a9ce38c4c100e52747dfd8c5f0cc9c86ecb4bb53
biancaasmart <biancaasmart@gmail.com>
2017-01-18 13:46:59 -0500

    Add files via upload

---
+++ WeirdWages.java
@@ -0,0 +1,18 @@
+import java.util.*;
+/**
+ * Write a description of class WeirdWages here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WeirdWages
+{
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Alrighty");
+        int 
+        if(
+    }
+}
+
